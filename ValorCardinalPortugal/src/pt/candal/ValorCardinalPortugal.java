9c1c36f08b544527751d3b0f1eddf36a99a6d538
Paulo Manuel Francisco Goncalves <paulomfgoncalves@hotmail.com>
2021-01-14 14:40:19 +0000

    add files

---
+++ ValorCardinalPortugal/src/pt/candal/ValorCardinalPortugal.java
@@ -0,0 +1,468 @@
+package pt.candal;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValorCardinalPortugal {
+
+	// campos em ordinal
+	
+	// xxx
+	private static final String[] CARDINAL_UNIDADES = { "", "um", "dois", "três", "quatro", "cinco", "seis", "sete",
+			"oito", "nove" };
+	private static final String[] CARDINAL_DEZENAS = { "", "dez", "vinte", "trinta", "quarenta", "cinquenta",
+			"sessenta", "setenta", "oitenta", "noventa" };
+	private static final String[] CARDINAL_DEZENAS_DEZ = { "dez", "onze", "doze", "treze", "catorze", "quinze",
+			"desasseis", "desassete", "dezoito", "dezanove" };
+	private static final String[] CARDINAL_CENTENAS = { "", "cento", "duzentos", "trezentos", "quatrocentos",
+			"quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos" };
+	private static final String CARDINAL_ZERO = "zero";
+	private static final String CARDINAL_UM = "um";
+	private static final String CARDINAL_UMA = "uma";
+	private static final String CARDINAL_CEM = "cem";
+
+	private static final String[] CARDINAL_GRUPOS_PLURAL = { "", "mil", "milhões", "milhares de milhão", "biliões",
+			"dezenas de bilião", "centenas de bilião", "milhares de bilião", "dezenas de milhar de bilião",
+			"centenas de milhar de bilião", "triliões" };
+
+	private static final String[] CARDINAL_GRUPOS_SINGULAR = { "", "mil", "milhão", "milhar de milhão", "bilião",
+			"dezena de bilião", "centena de bilião", "milhar de bilião", "dezena de milhar de bilião",
+			"centena de milhar de bilião", "trilião" };
+
+	private static final boolean[] CARDINAL_GRUPOS_MASCULINO = { true, true, true, true, true, false, false, true,
+			false, false, true };
+
+	// xxx.000
+	private static final String CARDINAL_MILHARES = "mil";
+
+	// xxx.000.000
+	private static final String CARDINAL_MILHOES_P = "milhões";
+	private static final String CARDINAL_MILHOES_1 = "milhão";
+
+	// xxx.000.000.000
+	private static final String CARDINAL_MILHAR_MILHOES_P = "milhares de milhão";
+	private static final String CARDINAL_MILHAR_MILHOES_1 = "milhar de milhão";
+
+	// xxx.000.000.000.000
+	private static final String CARDINAL_BILIOES_P = "biliões";
+	private static final String CARDINAL_BILIOES_1 = "bilião";
+	private static final String CARDINAL_DEZENAS_BILIOES_P = "dezenas de bilião";
+	private static final String CARDINAL_DEZENAS_BILIOES_1 = "dezena de bilião";
+	private static final String CARDINAL_CENTENA_BILIOES_P = "centenas de bilião";
+	private static final String CARDINAL_CENTENA_BILIOES_1 = "centena de bilião";
+
+	// xxx.000.000.000.000.000
+	private static final String milhar_bilioes_P = "milhares de bilião";
+	private static final String milhar_bilioes_1 = "milhar de bilião";
+
+	// confirmar
+	private static final String dezenas_milhar_bilioes_P = "dezenas de milhar de bilião";
+	private static final String dezenas_milhar_bilioes_1 = "dezena de milhar de bilião";
+	// confirmar
+	private static final String centena_milhar_bilioes_P = "centenas de milhar de bilião";
+	private static final String centena_milhar_bilioes_1 = "centena de milhar de bilião";
+
+	// xxx.000.000.000.000.000.000
+	private static final String CARDINAL_TRILIOES_P = "triliões";
+	private static final String CARDINAL_TRILIOES_1 = "trilião";
+
+	// milhar = (1 + 3 zeros)
+	// milhões = (1 + 6 zeros)
+	// mil milhões= (1 + 9 zeros)
+	// bilião = um milhão de milhões (1 + 12 zeros)
+	// trilião = um milhão de biliões (1 + 18 zeros)
+	// quatrilião = um milhão de triliões (1 + 24 zeros);
+	// quintilião = um milhão de quatriliões (1 + 30 zeros);
+	// sextilião = um milhão de quintiliões (1 + 36 zeros);
+	// septilião = um milhão de sextiliões (1 + 42 zeros);
+	// octilião = um milhão de septiliões (1 + 48 zeros);
+	// nonilião = um milhão de octiliões (1 + 54 zeros).
+
+	private static final String FRASE_SUFIXO_AO = "ão";
+	private static final String FRASE_SUFIXO_OES = "ões";
+
+	private static final String FRASE_E = " e ";
+	private static final String FRASE_VIRGULA = ", ";
+	private static final String FRASE_DE = " de";
+	private static final String FRASE_NOME_INTEIROS = "euros";
+	private static final String FRASE_NOME_INTEIROS_1 = "euro";
+	private static final String FRASE_NOME_DECIMAIS = "centimos";
+	private static final String FRASE_NOME_DECIMAIS_1 = "centimo";
+
+	public String Converte(BigDecimal valor) {
+
+		return Converte(valor, false, false);
+	}
+
+	public String Converte(BigDecimal valor, boolean vazioSeZeroParteinteira) {
+
+		return Converte(valor, vazioSeZeroParteinteira, false);
+	}
+
+	public String Converte(BigDecimal valor, boolean vazioSeZeroParteinteira, boolean vazioSeZeroParteDecimail) {
+
+		BigDecimal bigDecimal = valor.setScale(2, RoundingMode.FLOOR);
+		String temp = bigDecimal.toString();
+
+		return Converte(temp, vazioSeZeroParteinteira, vazioSeZeroParteDecimail);
+	}
+
+	public String Converte(String valor) {
+
+		return Converte(valor, false, false);
+	}
+
+	public String Converte(String valor, boolean vazioSeZeroParteinteira) {
+
+		return Converte(valor, vazioSeZeroParteinteira, false);
+	}
+
+	public String Converte(String valor, boolean vazioSeZeroParteinteira, boolean vazioSeZeroParteDecimail) {
+
+		String[] partes = DivideEmPartesInteiraBigDecimal(valor);
+
+		// separa por grupos de mil "???"
+		String[] gruposInteiros = DivideEmGruposDeMil(partes[0]);
+		String[] gruposDecimais = DivideEmGruposDeMil(partes[1]);
+
+		// descodifica os grupos inteiros
+		String[] gruposCardinaisInteiros = new String[gruposInteiros.length];
+		for (int x = 0; x < gruposInteiros.length; x++)
+			gruposCardinaisInteiros[x] = DescodificaCardinal(gruposInteiros[x], (gruposInteiros.length - x - 1));
+
+		// descodifica os groupos decimais
+		String[] gruposCardinaisDecimais = new String[gruposDecimais.length];
+		for (int x = 0; x < gruposDecimais.length; x++)
+			gruposCardinaisDecimais[x] = DescodificaCardinal(gruposDecimais[x], (gruposDecimais.length - x - 1));
+
+		// junta todos os grupos
+		String finalInteiros = JuntaTodosGruposDeMil(gruposCardinaisInteiros, vazioSeZeroParteinteira);
+		String finalDecimais = JuntaTodosGruposDeMil(gruposCardinaisDecimais, vazioSeZeroParteDecimail);
+
+		// caso: se valor = 0.0 mostra sempre "zero"
+		if ((finalInteiros.length() == 0) && (finalDecimais.length() == 0))
+			finalInteiros = CARDINAL_ZERO + " " + FRASE_NOME_INTEIROS;
+
+		// caso: analiza se coloca "de" ou "e" antes do qualificador
+		if (finalInteiros.length() > 2) {
+			// C# String temp = finalInteiros.SubString((finalInteiros.length() - 3), 3);
+			String temp = finalInteiros.substring((finalInteiros.length() - 3), finalInteiros.length());
+			if (temp.equals(FRASE_SUFIXO_OES))
+				finalInteiros += FRASE_DE;
+			else {
+				// C# temp = finalInteiros.SubString((finalInteiros.length() - 2), 2);
+				temp = finalInteiros.substring((finalInteiros.length() - 2), finalInteiros.length());
+				if (temp.equals(FRASE_SUFIXO_AO))
+					finalInteiros += FRASE_DE;
+			}
+		}
+
+		// obtem qualificadores
+		String qualificadorInteiros = ObtemQualificadorParteInteira(partes[0], vazioSeZeroParteinteira);
+		String qualificadoreDecimais = ObtemQualificadorParteBigDecimal(partes[1], vazioSeZeroParteDecimail);
+
+		// caso: adiciona qualificador inteiros
+		if (finalInteiros.length() > 0)
+			finalInteiros += " " + qualificadorInteiros;
+
+		// caso: adiciona qualificador decimais
+		if (finalDecimais.length() > 0)
+			if (finalInteiros.length() > 0)
+				finalDecimais += " ";
+		finalDecimais += qualificadoreDecimais;
+
+		// case: adiciona " e " entre a frase inteiros & frase decimais
+		String dual = "";
+		if ((finalInteiros.length() > 0) && (finalDecimais.length() > 0))
+			dual = FRASE_E;
+
+		return finalInteiros + dual + finalDecimais;
+	}
+
+	private String[] DivideEmPartesInteiraBigDecimal(String valor) {
+
+		String temp = valor;
+
+		if (temp.indexOf(".") == -1)
+			temp += ".00";
+
+		String[] partes = temp.split("\\.");
+
+		if (partes[0].length() == 0)
+			partes[0] = "0";
+
+		if (partes[1].length() == 0)
+			partes[1] = "00";
+		else if (partes[1].length() == 1)
+			partes[1] += "0";
+
+		return partes;
+	}
+
+	private String[] DivideEmGruposDeMil(String valor) {
+
+		String temp = valor;
+
+		// extrai
+		List<String> list = new ArrayList<String>();
+		while (temp.length() > 3) {
+			// C# String str3 = temp.SubString(temp.length() - 3);
+			String str3 = temp.substring(temp.length() - 3);
+			list.add(str3);
+			// C# temp = temp.SubString(0, temp.length() - 3);
+			temp = temp.substring(0, temp.length() - 3);
+		}
+		list.add(String.format("%1$3s", temp).replace(' ', '0') // garante comprimento = 3
+		// temp.PadLeft(3, '0')
+		);
+
+		// reverte array
+		int count = list.size();
+		String[] groupos = new String[count];
+		for (int x = 0; x < count; x++)
+			groupos[count - 1 - x] = list.get(x);
+
+		return groupos;
+	}
+
+	private String JuntaTodosGruposDeMil(String[] grouposEmCardinal, boolean vazioSeZero) {
+
+		String resultado = "";
+
+		for (int x = 0; x < grouposEmCardinal.length; x++) {
+
+			if (grouposEmCardinal[x].length() == 0)
+				continue;
+
+			// analisa se coloca o ultimo " e "
+			if ((x == (grouposEmCardinal.length - 1)) && (resultado.length() > 1)) {
+				int pos = grouposEmCardinal[x].indexOf(FRASE_E);
+				if (pos == -1) {
+					resultado = RemoveUltimasVirgulasEmExcesso(resultado);
+					// result = result.SubString(0, result.Length() - 2);
+					resultado += FRASE_E;
+				}
+			}
+
+			resultado += grouposEmCardinal[x];
+			resultado += FRASE_VIRGULA;
+		}
+
+		if (resultado.length() == 0) {
+			if (vazioSeZero)
+				return "";
+			else
+				return CARDINAL_ZERO;
+		}
+
+		resultado = RemoveUltimasVirgulasEmExcesso(resultado);
+
+		return resultado;
+	}
+
+	private String RemoveUltimasVirgulasEmExcesso(String valor) {
+
+		String resultado = valor;
+
+		// C# while (resultado.SubString(resultado.length() - 2, 2) == FRASE_VIRGULA)
+		while (resultado.substring(resultado.length() - 2, resultado.length()).equals(FRASE_VIRGULA))
+			// C# resultado = resultado.SubString(0, resultado.length() - 2);
+			resultado = resultado.substring(0, resultado.length() - 2);
+
+		return resultado;
+	}
+
+	private String DescodificaCardinal(String valor, int nivel) {
+
+		if (valor.equals("000"))
+			return "";
+
+		String[] cardinalArray = new String[3];
+		byte[] digitArray = new byte[3];
+
+		for (byte x = 0; x < 3; x++)
+			// C# digitArray[x] = Byte.parseByte(valor.SubString(x, 1));
+			digitArray[x] = Byte.parseByte(valor.substring(x, (x + 1)));
+
+		cardinalArray[0] = ObtemCentenas(digitArray[0], digitArray[1], digitArray[2]);
+		cardinalArray[1] = ObtemDezenas(digitArray[1], digitArray[2]);
+		cardinalArray[2] = ObtemUnidades(digitArray[2], digitArray[1]);
+
+		String resultado = JuntaCentenasDezenasUnidades(cardinalArray[0], cardinalArray[1], cardinalArray[2]);
+
+		resultado = AdicionaSufixoDeGrupoMil(resultado, nivel);
+
+		return resultado;
+	}
+
+	private String JuntaCentenasDezenasUnidades(String centena, String dezena, String unidade) {
+
+		String resultado = centena;
+		if ((centena.length() > 0) && ((dezena.length() > 0) || (unidade.length() > 0)))
+			resultado += FRASE_E;
+
+		resultado += dezena;
+		if ((dezena.length() > 0) && (unidade.length() > 0))
+			resultado += FRASE_E;
+
+		resultado += unidade;
+
+		return resultado;
+	}
+
+	private String ObtemUnidades(byte digito, byte dezena) {
+
+		if (dezena == 1)
+			return "";
+
+		return CARDINAL_UNIDADES[digito];
+	}
+
+	private String ObtemDezenas(byte digito, byte unidade) {
+
+		if (digito == 1)
+			return CARDINAL_DEZENAS_DEZ[unidade];
+
+		return CARDINAL_DEZENAS[digito];
+	}
+
+	private String ObtemCentenas(byte digito, byte dezena, byte unidade) {
+
+		if ((digito == 1) && (dezena == 0) && (unidade == 0))
+			return CARDINAL_CEM; // Caso : Cem
+
+		return CARDINAL_CENTENAS[digito];
+	}
+
+	private String ObtemQualificadorParteBigDecimal(String valor, boolean vazioSeZero) {
+
+		byte valTemp = Byte.parseByte(valor);
+
+		if (valTemp > 1)
+			return FRASE_NOME_DECIMAIS;
+
+		if (valTemp == 1)
+			return FRASE_NOME_DECIMAIS_1;
+
+		if (!vazioSeZero && (valTemp == 0))
+			return FRASE_NOME_DECIMAIS;
+
+		return "";
+	}
+
+	private String ObtemQualificadorParteInteira(String valor, boolean vazioSeZero) {
+
+		double valTemp = Double.parseDouble(valor);
+
+		if (valTemp > 1)
+			return FRASE_NOME_INTEIROS;
+
+		if (valTemp == 1)
+			return FRASE_NOME_INTEIROS_1;
+
+		if (!vazioSeZero && (valTemp == 0))
+			return FRASE_NOME_INTEIROS;
+
+		return "";
+	}
+
+	private String AdicionaSufixoDeGrupoMil(String valor, int nivel) {
+		String resultado = "";
+
+		// TODO: usar CARDINAL_GROUPS[]
+		switch (nivel) {
+
+		case 0: // xxx -unidades, dezenas, centenas
+			resultado = valor;
+			break;
+
+		case 1: // xxx.000 - milhares
+			if (valor.equals(CARDINAL_UM))
+				resultado = CARDINAL_MILHARES; // special : remove palavra "um" (um mil)
+			else
+				resultado += valor + " " + CARDINAL_MILHARES;
+			break;
+
+//		case 2: // milhoes xxx.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UM + " " + CARDINAL_MILHOES_1;
+//			else
+//				resultado += valor + " " + CARDINAL_MILHOES_P;
+//			break;
+//
+//		case 3: // milhar de milhoes xxx.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UM + " " + CARDINAL_MILHAR_MILHOES_1;
+//			else
+//				resultado += valor + " " + CARDINAL_MILHAR_MILHOES_P;
+//			break;
+//
+//		case 4: // bilioes xxx.000.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UM + " " + CARDINAL_BILIOES_1;
+//			else
+//				resultado += valor + " " + CARDINAL_BILIOES_P;
+//			break;
+//
+//		case 5: // dezena de bilioes xxx-000.000.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UMA + " " + CARDINAL_DEZENAS_BILIOES_1;
+//			else
+//				resultado += valor + " " + CARDINAL_DEZENAS_BILIOES_P;
+//			break;
+//
+//		case 6: // centena de bilioes xxx.000.000.000.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UMA + " " + CARDINAL_CENTENA_BILIOES_1;
+//			else
+//				resultado += valor + " " + CARDINAL_CENTENA_BILIOES_P;
+//			break;
+//
+//		case 7: // milhar de bilião xxx.000.000.000,000.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UM + " " + milhar_bilioes_1;
+//			else
+//				resultado += valor + " " + milhar_bilioes_P;
+//			break;
+//
+//		case 8: // dezena de milhar de bilião xxx.000.000.000,000.000.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UMA + " " + dezenas_milhar_bilioes_1;
+//			else
+//				resultado += valor + " " + dezenas_milhar_bilioes_P;
+//			break;
+//
+//		case 9: // centena de milhar de bilião xxx.000.000.000,000.000.000.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UMA + " " + centena_milhar_bilioes_1;
+//			else
+//				resultado += valor + " " + centena_milhar_bilioes_P;
+//			break;
+//
+//		case 10: // trilião xxx.000.000.000,000.000.000.000.000.000.000
+//			if (valor.equals(CARDINAL_UM))
+//				resultado = CARDINAL_UM + " " + CARDINAL_TRILIOES_1;
+//			else
+//				resultado += valor + " " + CARDINAL_TRILIOES_P;
+//			break;
+
+		default:
+
+			if (valor == CARDINAL_UM) {
+				if (CARDINAL_GRUPOS_MASCULINO[nivel])
+					resultado = CARDINAL_UM;
+				else
+					resultado = CARDINAL_UMA;
+
+				resultado += " " + CARDINAL_GRUPOS_SINGULAR[nivel];
+			} else
+				resultado += valor + " " + CARDINAL_GRUPOS_PLURAL[nivel];
+			break;
+		}
+
+		return resultado;
+	}
+
+}
---
+++ ValorCardinalPortugalCall/src/pt/candal/Program.java
@@ -0,0 +1,57 @@
+package pt.candal;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+//		 //double operation = 890.0 / 1440.0;
+//		 //BigDecimal big = new BigDecimal(operation);
+//		double ab = Double.parseDouble("1234567890123456789123456789012345678901234567890.99");  
+//		System.out.println(String.format("double : %s", ab));
+//		
+//		 //BigDecimal big = new BigDecimal("99999999999999999999999999.99"); //max c#
+//		 BigDecimal big = new BigDecimal("1234567890123456789123456789012345678901234567890.99");
+//		                                  
+//		 
+//		 System.out.println(String.format("operation : %s", big));
+//		 
+//		 BigDecimal big3 = new BigDecimal(ab);      // double to dicimal  
+//		 BigDecimal big2 = BigDecimal.valueOf(ab);  // double to dicimal      
+//		 big2 = big2.setScale(2, RoundingMode.FLOOR);        
+//		 System.out.println(String.format("operation : %s", big2));
+//
+//		 double d2 = big2.doubleValue(); // dicimal to double
+//         
+//         //System.out.println(String.format("operation : %s", operation));
+//         System.out.println(String.format("scaled : %s", d2));
+//		
+//		//BigInteger cc = new BigInteger(0);
+//
+//		//decimal dec = 1.9;
+//		
+//		
+//		BigDecimal aa = new BigDecimal(1.19);
+//		BigInteger bb = aa.toBigInteger();
+//		BigInteger cc = aa.toBigIntegerExact();
+//
+//		String xx= "1010110";
+//		
+//		//String yyy = String.format("%03d", xx); //String.format("%1$3s", xx);
+//		//String yyy =   String.format("%05s", xx);
+//		             
+//		String yyy = String.format("%1$3s", xx).replace(' ', '0');
+//		yyy=yyy;
+//		
+		
+
+		ValorCardinalPortugal xpto = new ValorCardinalPortugal();
+		String result = xpto.Converte("12345678901234567890.99");
+		 System.out.println(result);
+		
+	}
+
+}
